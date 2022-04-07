e939ff4ee1399ae56cb65243e2a8f656da110d12
Richie-Yang <richieyang8813@gmail.com>
2022-04-07 14:34:12 +0800

    feat: add both intervalTimer and timeoutTimer for the banner action

--- routes/index.ts
+++ routes/index.ts
@@ -1,33 +1,38 @@
 const router = require('express').Router()
+const base64 = require('base-64')
 const mathHelpers = require('../helpers/math-helpers')
+let token: string = ''
 
 router.get('/', (req: any, res: any) => {
   const question = mathHelpers.generateQuestion()
+  token = base64.encode(question)
   res.render('index', { question })
 })
 
 router.post('/submit', (req: any, res: any) => {
   const { question, answer } = req.body
+  const nextQuestion = mathHelpers.generateQuestion() 
   const response = { 
-    status: 'error',
+    status: 'Error!',
     message: 'none',
-    question: mathHelpers.generateQuestion() 
+    question: nextQuestion
   }
+
+  if (base64.encode(question) !== token) {
+    response.message = 'Something went wrong'
+    return res.send(response)
+  }
+  token = base64.encode(nextQuestion)
   
   if (isNaN(Number(answer)) || answer === '') {
     response.message = 'Please input number format'
     return res.send(response)
   } 
 
-  if (!question?.trim()) {
-    response.message = 'Something went wrong'
-    return res.send(response)
-  }
-
   const calculatedAnswer: number = mathHelpers.calculateAnswer(question)
   const CorrectOrWrong: string = calculatedAnswer === Number(answer)
-    ? 'correct'
-    : 'incorrect'
+    ? 'Correct!'
+    : 'Incorrect!'
   
   response.status = CorrectOrWrong
   response.message = `Correct answer is ${calculatedAnswer}\nYour Answer is ${CorrectOrWrong}`
